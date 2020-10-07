18f89688fb447de57a649d4c7f73542803696a7e
Teresa Strout <qapla47@gmail.com>
2020-10-07 09:26:51 -0400

    initial commit

---
+++ skipValidation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+WEBUI_PATTERN='lead_manager_react_django/apps/webui'
+
+if git diff --name-only | grep -q WEBUI_PATTERN; then
+    echo 'found match'
+    yarn run validate:src
+    exit 0
+else 
+    echo 'no match'
+    exit 0
+fi
